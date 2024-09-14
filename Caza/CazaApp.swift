114bba06c703a2eb65211799efc0a0995b8108cf
Cristobal Ramos <cristobal.ramos@vodafone.com>
2024-09-14 18:00:41 +0200

    Initial Commit

---
+++ Caza/CazaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CazaApp.swift
+//  Caza
+//
+//  Created by Cristobal Ramos on 14/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CazaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Caza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Caza
+//
+//  Created by Cristobal Ramos on 14/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
