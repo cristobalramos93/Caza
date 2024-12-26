ae2f0d4266f95733d935ae59f823b37bf757c5a0
Cristobal Ramos <cristobal.ramos@vodafone.com>
2024-12-26 17:05:00 +0100

    error boton ver errores iphone 15 pro max

--- Caza/ResultView.swift
+++ Caza/ResultView.swift
@@ -11,114 +11,116 @@ struct ResultView: View {
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
-        ZStack {
-            // Fondo que cambia entre verde y rojo
-            (result.success ? Color.green.opacity(0.3) : Color.red.opacity(0.3))
-                .ignoresSafeArea()
-            
-            VStack(spacing: 40) {
-                // Texto central de aprobado o no
-                Text(result.success ? "¡Has aprobado!" : "No has aprobado")
-                    .font(.system(size: 36, weight: .bold, design: .rounded))
-                    .foregroundColor(result.success ? Color.green : Color.red)
-                    .padding()
-                    .background(Color.white.opacity(0.8))
-                    .cornerRadius(15)
-                    .shadow(radius: 10)
+        NavigationView {
+            ZStack {
+                // Fondo que cambia entre verde y rojo
+                (result.success ? Color.green.opacity(0.3) : Color.red.opacity(0.3))
+                    .ignoresSafeArea()
                 
-                // Vista de resultados con cantidad de respuestas
-                VStack(spacing: 20) {
-                    HStack {
-                        VStack {
-                            Text("Acertadas")
-                                .font(.title2)
-                                .foregroundColor(.green)
-                            Text("\(result.correctAnswers)")
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                        }
-                        
-                        Spacer()
-                        
-                        VStack {
-                            Text("Falladas")
-                                .font(.title2)
-                                .foregroundColor(.red)
-                            Text("\(result.incorrectAnswers)")
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                        }
-                    }
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(15)
-                    .shadow(radius: 10)
-                }
-                .padding(.horizontal)
-                HStack(spacing: 30) {
-                    Button(action: {
-                        dismiss()
-                        repeatt()
-                    }) {
-                        Text("Repetir")
-                            .font(.title2)
-                            .bold()
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.blue.opacity(0.8))
-                            .foregroundColor(.white)
-                            .cornerRadius(15)
-                            .shadow(radius: 5)
-                    }
+                VStack(spacing: 40) {
+                    // Texto central de aprobado o no
+                    Text(result.success ? "¡Has aprobado!" : "No has aprobado")
+                        .font(.system(size: 36, weight: .bold, design: .rounded))
+                        .foregroundColor(result.success ? Color.green : Color.red)
+                        .padding()
+                        .background(Color.white.opacity(0.8))
+                        .cornerRadius(15)
+                        .shadow(radius: 10)
                     
-                    NavigationLink(destination: ShowErrors(exam: exam, answerIndex: answerIndex, isCorrect: isCorrect, answer: answer)) {
-                        Text("Ver errores")
-                            .font(.title2)
-                            .bold()
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.orange.opacity(0.8))
-                            .foregroundColor(.white)
-                            .cornerRadius(15)
-                            .shadow(radius: 5)
+                    // Vista de resultados con cantidad de respuestas
+                    VStack(spacing: 20) {
+                        HStack {
+                            VStack {
+                                Text("Acertadas")
+                                    .font(.title2)
+                                    .foregroundColor(.green)
+                                Text("\(result.correctAnswers)")
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                            }
+                            
+                            Spacer()
+                            
+                            VStack {
+                                Text("Falladas")
+                                    .font(.title2)
+                                    .foregroundColor(.red)
+                                Text("\(result.incorrectAnswers)")
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                            }
+                        }
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(15)
+                        .shadow(radius: 10)
                     }
+                    .padding(.horizontal)
+                    HStack(spacing: 30) {
+                        Button(action: {
+                            dismiss()
+                            repeatt()
+                        }) {
+                            Text("Repetir")
+                                .font(.title2)
+                                .bold()
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.blue.opacity(0.8))
+                                .foregroundColor(.white)
+                                .cornerRadius(15)
+                                .shadow(radius: 5)
+                        }
+                        NavigationLink(destination: ShowErrors(exam: exam, answerIndex: answerIndex, isCorrect: isCorrect, answer: answer)) {
+                            Text("Ver errores")
+                                .font(.title2)
+                                .bold()
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.orange.opacity(0.8))
+                                .foregroundColor(.white)
+                                .cornerRadius(15)
+                                .shadow(radius: 5)
+                        }
+                    }
+                    .padding(.horizontal)
+                    
+                    // Botones adicionales
+                    HStack(spacing: 30) {
+                        NavigationLink(destination: Menu()) {
+                            Text("Ir al Menú")
+                                .font(.title2)
+                                .bold()
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.gray.opacity(0.8))
+                                .foregroundColor(.white)
+                                .cornerRadius(15)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Button(action: {
+                            startNewExam() // Acción para iniciar un nuevo examen
+                            dismiss()
+                        }) {
+                            Text("Hacer Otro Examen")
+                                .font(.title2)
+                                .bold()
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.green.opacity(0.8))
+                                .foregroundColor(.white)
+                                .cornerRadius(15)
+                                .shadow(radius: 5)
+                        }
+                    }
+                    .padding(.horizontal)
                 }
-                .padding(.horizontal)
-                
-                // Botones adicionales
-                HStack(spacing: 30) {
-                    NavigationLink(destination: Menu()) {
-                        Text("Ir al Menú")
-                            .font(.title2)
-                            .bold()
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.gray.opacity(0.8))
-                            .foregroundColor(.white)
-                            .cornerRadius(15)
-                            .shadow(radius: 5)
-                    }
-
-                    Button(action: {
-                        startNewExam() // Acción para iniciar un nuevo examen
-                        dismiss()
-                    }) {
-                        Text("Hacer Otro Examen")
-                            .font(.title2)
-                            .bold()
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.green.opacity(0.8))
-                            .foregroundColor(.white)
-                            .cornerRadius(15)
-                            .shadow(radius: 5)
-                    }
-                }
-                .padding(.horizontal)
+                .padding()
             }
-            .padding()
+            .navigationBarBackButtonHidden()
         }
-        .navigationBarBackButtonHidden()
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
